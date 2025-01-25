285f086ffc5596ddddc225f065fc31faa1dde150
Bernardo <beprieto47@gmail.com>
2025-01-25 15:06:30 -0300

    Adição do método para remover pessoas de dentro do elevador

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        Elevator elevator = new Elevator(3);
+        Elevator elevator = new Elevator(6);
 
         int cannotEnter = 0;
 
-        cannotEnter += elevator.addPassenger(1);
-        System.out.println(cannotEnter);
-
-        cannotEnter += elevator.addPassenger(1);
+        cannotEnter += elevator.addPassenger(4);
         System.out.println(cannotEnter);
 
         cannotEnter += elevator.addPassenger(3);
 
         System.out.println("Não conseguiram entrar " + cannotEnter);
+
+        elevator.removePassenger(4);
+
+        elevator.addPassenger(cannotEnter);
 
         System.out.println("Total de passageiros dentro do elevador " + elevator.getCurrentPassenger());
     }
